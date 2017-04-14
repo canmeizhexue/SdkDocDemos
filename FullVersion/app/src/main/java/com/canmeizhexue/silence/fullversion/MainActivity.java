4a639a1b173713dec74e8661fdab32d225282d4b
zengyaping <zengyaping@htmimi.com>
2017-04-14 15:31:59 +0800

    [增]静态代码检查

---
+++ FullVersion/app/src/main/java/com/canmeizhexue/silence/fullversion/MainActivity.java
@@ -0,0 +1,16 @@
+package com.canmeizhexue.silence.fullversion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * MainActivity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ FullVersion/app/src/main/java/com/canmeizhexue/silence/fullversion/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.canmeizhexue.silence.fullversion;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
