8f5ad96f4d8ace45fe66a4d94c42b528daa4d315
zengyaping <zengyaping@htmimi.com>
2017-04-14 15:33:19 +0800

    [增]so测试

---
+++ AndroidStudio/NewProjectWithC/app/src/main/java/com/canmeizhexue/newprojectwithc/SoTest.java
@@ -0,0 +1,13 @@
+package com.canmeizhexue.newprojectwithc;
+
+/**
+ * Created by zengyaping on 2017-4-13.
+ */
+
+public class SoTest {
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public static native String stringFromJNI();
+}
